0729cdfa99fb6205055d0675b17a74ad7e1a0c44
Jamee Kim <jameekim@nodaplife.me>
2018-10-08 18:22:01 +0900

    run test from single file

---
+++ test/compiler.ts
@@ -0,0 +1,7 @@
+// import { buildAst } from "../src/compiler/language/buildAst.spec";
+import { getToken } from "../src/compiler/language/getToken.spec";
+
+describe("Source Code Parsing", function() {
+  getToken();
+  // buildAst();
+});
