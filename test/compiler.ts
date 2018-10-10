5433400f3e058617e87cd9ca3be3da8d46d10bca
Jamee Kim <jameekim@nodaplife.me>
2018-10-11 03:30:00 +0900

    test: move lib tests to a dedicated folder

--- test/compiler.ts
+++
@@ -1,9 +0,0 @@
-import { testBuildAst } from "../src/compiler/language/buildAst.spec";
-import { testGetTokens } from "../src/compiler/language/getTokens.spec";
-
-describe("Compiler", function() {
-  describe("Source Code Parsing", function() {
-    testGetTokens();
-    testBuildAst();
-  });
-});
---
+++ test/lib/compiler.ts
@@ -0,0 +1,9 @@
+import { testBuildAst } from "../../src/compiler/language/buildAst.spec";
+import { testGetTokens } from "../../src/compiler/language/getTokens.spec";
+
+describe("Compiler", function() {
+  describe("Source Code Parsing", function() {
+    testGetTokens();
+    testBuildAst();
+  });
+});
